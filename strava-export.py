219f64936d1c94fe093aaa4a7f1304266e016752
mookie kong <ultramookie@ymail.com>
2014-09-28 17:33:42 -0700

    added monthly distance totals. added lifetime stats.

--- strava-export.py
+++ strava-export.py
@@ -11,6 +11,9 @@ page=0
 per_page=200
 num_activities=per_page
 current_month = None
+current_month_mileage = 0
+all_time_mileage = 0
+all_time_seconds = 0
 
 base_url = 'https://www.strava.com/api'
 url = "/v3/activities/?access_token=" + ACCESS_TOKEN + "&per_page=" + str(per_page) + "&page=" + str(page)
@@ -37,6 +40,10 @@ while num_activities == per_page:
 		if month != current_month:
 			current_month = month
 			file.write('\n')
+			if current_month_mileage > 0:
+				file.write('Total Distance: ' + current_month_distance + ' miles\n')
+				file.write('\n')
+				current_month_mileage = 0
 			file.write('--' + current_month + '--\n')
 		
 		elevationgain = activity.get('total_elevation_gain')		
@@ -47,9 +54,14 @@ while num_activities == per_page:
 		maxspeed = '%.2f' % round(float(activity.get('max_speed') * 2.2369362920544),2)
 		# convert from km to mi and round
 		miles = float(activity.get('distance')) * 0.000621371
+		current_month_mileage = current_month_mileage + miles
+		all_time_mileage = all_time_mileage + miles
+		all_time_distance = '%.2f' % round(all_time_mileage, 2)
+		current_month_distance = '%.2f' % round(current_month_mileage, 2)
 		distance = '%.2f' % round(miles, 2)
 
 		duration_seconds = activity.get('moving_time')
+		all_time_seconds = all_time_seconds + duration_seconds
 
 		pace = ''
 		if miles > 0:
@@ -64,4 +76,26 @@ while num_activities == per_page:
 
 		file.write(str(date) + " : " + distance.ljust(5) + "mi " + pace.ljust(11) + "" + str(durhours).rjust(2, '0') + ":" + str(durmins).rjust(2,'0') + ":" + str(dursecs).rjust(2, '0') + " " + str(avgheartrate).ljust(5) + " " + str(maxheartrate).ljust(5) + " " + avgspeed.ljust(5) + " " + maxspeed.ljust(5) + " " + str(avgcadence).ljust(6) + " " + str(elevationgain).ljust(6) + "\n")
 
+
+pace = ''
+if miles > 0:
+	seconds_per_mile = all_time_seconds / all_time_mileage
+else:
+	seconds_per_mile = 0
+hours, remainder = divmod(seconds_per_mile, 3600)
+minutes, seconds = divmod(remainder, 60)
+pace = '(%.0f\'%02.0f/mi)' % (minutes, seconds)
+durhours, durrem = divmod(all_time_seconds, 3600)
+durmins, dursecs = divmod(durrem,60)
+
+file.write('\n')
+file.write('Total Distance: ' + current_month_distance + ' miles\n')
+file.write('\n')
+file.write('----------------\n')
+file.write('All Time Metrics\n')
+file.write('----------------\n')
+file.write('Distance: ' + all_time_distance + ' miles\n')
+file.write('Pace: ' + pace + '\n')
+file.write('Duration: ' + str(durhours) + ":" + str(durmins) + ":" + str(dursecs) + '\n')
+file.write('----------------\n')
 file.close()
