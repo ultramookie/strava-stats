91150b0db811109d9e4b95bbd9478073e9f4b63c
mookie kong <ultramookie@ymail.com>
2014-10-15 07:45:08 -0700

    adding json dumping. will dump individual activities to individual json files for consumption via things like splunk.

---
+++ strava-json.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import json
+import urllib
+import urllib2
+import time
+import os.path
+ 
+ACCESS_TOKEN = ''
+ 
+page=0
+per_page=200
+jsondir = 'strava-json/'
+
+num_activities=per_page
+
+base_url = 'https://www.strava.com/api'
+url = "/v3/activities/?access_token=" + ACCESS_TOKEN + "&per_page=" + str(per_page) + "&page=" + str(page)
+
+while num_activities == per_page:
+	page=page+1
+	url = "/v3/activities/?access_token=" + ACCESS_TOKEN + "&per_page=" + str(per_page) + "&page=" + str(page)
+	req = urllib2.Request('%s%s' % (base_url, url), None)
+	r = urllib2.urlopen(req)
+	resp = json.loads(r.read())
+	r.close()
+	num_activities = len(resp)
+
+	for activity in resp:
+		id = str(activity.get('id'))
+		jsonfile = jsondir + id + ".json"
+		if not os.path.isfile(jsonfile):
+			file = open(jsonfile,'w+')
+			json.dump(activity,file)
+			file.close()
